807f3dd243dcc948d6d68ef1f003e142fc7dd118
lucasdelimagarreto <54108848+lucasdelimagarreto@users.noreply.github.com>
2023-10-15 20:00:16 -0300

    sad

--- Projetos/spring_mte/src/main/java/com/mte2023/spring_mte/config/TestConfig.java
+++ Projetos/spring_mte/src/main/java/com/mte2023/spring_mte/config/TestConfig.java
@@ -13,15 +13,15 @@ import com.mte2023.spring_mte.repositories.UserRepisitory;
 @Configuration
 @Profile("test")
 public class TestConfig implements CommandLineRunner {
-    
+
     @Autowired
     private UserRepisitory userRepisitory;
 
     @Override
     public void run(String... args) throws Exception {
-        User u1 = new User(null, null, null, null, null, null);
-        User u2 = new User(null, null, null, null, null, null);
-        
+        User u1 = new User(null, "Lucas", "lucas@email.com", "123456", "66666", null);
+        User u2 = new User(null, "Rafael", "Rafael@email.com", "123456", "66666", null);
+
         userRepisitory.saveAll(Arrays.asList(u1, u2));
     }
 
