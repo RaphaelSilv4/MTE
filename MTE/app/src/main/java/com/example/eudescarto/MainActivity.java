e4e0d9a9664729b1ef09d4d92cc55463f052f88f
Francileudo Oliveira <sfrancileudo1@gmail.com>
2023-10-18 18:28:29 -0300

    feat: login and signup messages added to the page and design adjustments

--- MTE/app/src/main/java/com/example/eudescarto/MainActivity.java
+++ MTE/app/src/main/java/com/example/eudescarto/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.eudescarto;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button meuBotao = findViewById(R.id.button2);
+        meuBotao.setBackgroundResource(R.drawable.button_selector);
+
+        meuBotao.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, sign_up.class);
+            startActivity(intent);
+        });
     }
 }
--- MTE/app/src/main/java/com/example/eudescarto/sign_up.java
+++ MTE/app/src/main/java/com/example/eudescarto/sign_up.java
@@ -2,7 +2,10 @@ package com.example.eudescarto;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+
 
 public class sign_up extends AppCompatActivity {
 
@@ -10,5 +13,14 @@ public class sign_up extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        Button meuBotao = findViewById(R.id.button2);
+        meuBotao.setBackgroundResource(R.drawable.button_selector);
+
+        meuBotao.setOnClickListener(v -> {
+            Intent intent = new Intent(sign_up.this, MainActivity.class);
+            startActivity(intent);
+        });
+
     }
 }
