95c913fa4918bda55306cfb8af2553c80b38e7d4
lucasdelimagarreto <54108848+lucasdelimagarreto@users.noreply.github.com>
2023-10-18 12:34:46 -0300

    teste

---
+++ Projetos/spring_mte/src/main/java/com/mte2023/spring_mte/config/TestConfig_Users.java
@@ -0,0 +1,26 @@
+package com.mte2023.spring_mte.config;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+import com.mte2023.spring_mte.entities.User;
+import com.mte2023.spring_mte.repositories.UserRepisitory;
+
+import org.springframework.transaction.annotation.Transactional; 
+/* Não mexam aqui, eu sem que tem coisas inutilizadas*/
+@Configuration
+@Transactional
+public class TestConfig_Users implements CommandLineRunner {
+    
+    @Autowired
+    private UserRepisitory userRepisitory;
+
+    @Override
+    public void run(String... args) throws Exception {
+
+        
+    }
+}
