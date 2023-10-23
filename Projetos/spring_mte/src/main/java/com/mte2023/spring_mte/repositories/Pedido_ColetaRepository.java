3b692d7eab26ef07819c9a0293cc772acd7329ac
lucasdelimagarreto <54108848+lucasdelimagarreto@users.noreply.github.com>
2023-10-23 18:30:12 -0300

    sad

--- Projetos/spring_mte/src/main/java/com/mte2023/spring_mte/repositories/Pedido_ColetaRepository.java
+++ Projetos/spring_mte/src/main/java/com/mte2023/spring_mte/repositories/Pedido_ColetaRepository.java
@@ -1,7 +1,5 @@
 package com.mte2023.spring_mte.repositories;
-
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import com.mte2023.spring_mte.entities.Pedido_Coleta;
 
 public interface Pedido_ColetaRepository extends JpaRepository<Pedido_Coleta, Long>{
