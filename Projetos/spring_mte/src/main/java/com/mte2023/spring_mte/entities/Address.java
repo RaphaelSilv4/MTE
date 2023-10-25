5a502cb60d6b70fbddbd6f8a2b144b25be5347bf
lucasdelimagarreto <54108848+lucasdelimagarreto@users.noreply.github.com>
2023-10-25 17:55:24 -0300

    sad

--- Projetos/spring_mte/src/main/java/com/mte2023/spring_mte/entities/Address.java
+++ Projetos/spring_mte/src/main/java/com/mte2023/spring_mte/entities/Address.java
@@ -6,12 +6,14 @@ import java.util.List;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
+@Entity
 @Table(name = "tb_address")
 public class Address implements Serializable{
     private static final long serialVersionUID = 1L;
