28de828e258868a9e32e0894ee9eb75217948cde
Francileudo Oliveira <sfrancileudo1@gmail.com>
2023-10-25 12:03:00 -0300

    feat: built bonus screen

--- MTE/app/src/main/java/com/example/eudescarto/bonus_screen.java
+++ MTE/app/src/main/java/com/example/eudescarto/bonus_screen.java
@@ -4,21 +4,23 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.ImageButton;
 
-public class profile extends AppCompatActivity {
+public class bonus_screen extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_profile);
+        setContentView(R.layout.activity_bonus_screen);
 
-        ImageButton myButton = findViewById(R.id.imageView3);
+        ImageButton myButton = findViewById(R.id.imageViewChevron);
         myButton.setBackgroundResource(R.drawable.button_selector);
 
         myButton.setOnClickListener(v -> {
-            Intent intent = new Intent(profile.this, bonus_screen.class);
+            Intent intent = new Intent(bonus_screen.this, profile.class);
             startActivity(intent);
         });
+
     }
 }
