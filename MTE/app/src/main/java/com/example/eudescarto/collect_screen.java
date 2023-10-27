fce7fe7009dda8de6a16a2f50978a41560632301
Francileudo Oliveira <sfrancileudo1@gmail.com>
2023-10-27 08:46:28 -0300

    feat: built garbage collection request screen

---
+++ MTE/app/src/main/java/com/example/eudescarto/collect_screen.java
@@ -0,0 +1,40 @@
+package com.example.eudescarto;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.ImageButton;
+
+public class collect_screen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_collect_screen);
+
+        ImageButton myButton = findViewById(R.id.imageViewChevron);
+        myButton.setBackgroundResource(R.drawable.button_selector);
+
+        ImageButton myButton2 = findViewById(R.id.imageView12);
+        myButton.setBackgroundResource(R.drawable.button_selector);
+
+        ImageButton myButton3 = findViewById(R.id.imageView15);
+        myButton.setBackgroundResource(R.drawable.button_selector);
+
+        myButton.setOnClickListener(v -> {
+            Intent intent = new Intent(collect_screen.this, profile.class);
+            startActivity(intent);
+        });
+
+        myButton2.setOnClickListener(v -> {
+            Intent intent = new Intent(collect_screen.this, profile.class);
+            startActivity(intent);
+        });
+
+        myButton3.setOnClickListener(v -> {
+            Intent intent = new Intent(collect_screen.this, config_screen.class);
+            startActivity(intent);
+        });
+    }
+}
