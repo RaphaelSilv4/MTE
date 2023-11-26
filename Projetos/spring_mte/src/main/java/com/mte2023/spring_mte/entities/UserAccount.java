41e44fc830eef1db5ec3cfd9b4ace293780301f4
Tharcio Camboim <89159550+TharcioCBM@users.noreply.github.com>
2023-11-26 13:59:00 -0300

    crud CollectionOrder

--- Projetos/spring_mte/src/main/java/com/mte2023/spring_mte/entities/UserAccount.java
+++ Projetos/spring_mte/src/main/java/com/mte2023/spring_mte/entities/UserAccount.java
@@ -8,6 +8,11 @@ import java.io.Serializable;
 @Table(name = "user_account")
 public class UserAccount implements Serializable{
 
-
+    @Id
+    @Column(name="user_id")
+    protected Integer userid;
+    @OneToOne
+    @PrimaryKeyJoinColumn(name="user_id", referencedColumnName = "id")
+    protected User owner;
 
 }
